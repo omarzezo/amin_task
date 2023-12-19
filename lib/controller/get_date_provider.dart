d16843c63ed31ed78814536bad67539c00e66f6c
Omar <zezoomar674@yahoo.com>
2023-12-19 22:24:01 +0200

    omar

--- lib/controller/get_date_provider.dart
+++ lib/controller/get_date_provider.dart
@@ -7,48 +7,29 @@ import 'package:flutter/material.dart';
 
 class GetDataProvider extends ChangeNotifier{
   Status ? status;
-  DbHelper ? dbHelper;
+  DbHelper ? dbHelper = DbHelper();
   DepositeModel ? depositeModel;
   final TextEditingController inputController = TextEditingController();
   String ? date;
 
-  Future<Response<dynamic>?> getData()async{
-    print('post Dio++++');
-    //  await HttpHelper().getDataHttp(date: inputController.text);
-    // await dbHelper?.getDataSource(date: inputController.text)
-    // .then((value) {
-    //   depositeModel = DepositeModel.fromJson(value.data);
-    //   status = Status.SUCCESS;
-    //   notifyListeners();
-    //   print('${depositeModel?.data.the20231212.length}');
-    // });
-    // notifyListeners();
-
-    try {
+  Future<DepositeModel> getData()async{
+    // Response? data;
+    // try {
       status = Status.LOADING;
       notifyListeners();
-      // await HttpHelper().getDataHttp(date: inputController.text)
-      await dbHelper?.getDataSource(date: inputController.text)
-          .then((value) {
-        depositeModel = DepositeModel.fromJson(value?.data);
-        // depositeModel =depositeModelFromJson(value.data);
-        status = Status.SUCCESS;
-        notifyListeners();
-        print('${depositeModel?.data.the20231212.length}');
-      });
-      notifyListeners();
-      // if (response.statusCode == 200) {
-      //   print(response.statusMessage);
-      //   status = Status.SUCCESS;
-      //   notifyListeners();
-      //   return response;
-      // }
-    } catch (e) {
-      debugPrint(e.toString());
-      status = Status.ERROR;
-      notifyListeners();
-    }
 
+      DepositeModel? date= await dbHelper?.getDataOmar(date:inputController.text.isEmpty?'12':inputController.text);
+      print("date>>"+date!.message.toString());
+      // depositeModel = DepositeModel.fromJson(date?.data);
+      // status = Status.SUCCESS;
+      // notifyListeners();
+      // notifyListeners();
+    // } catch (e) {
+    //   debugPrint(e.toString());
+    //   status = Status.ERROR;
+    //   notifyListeners();
+    // }
+    return date!;
     notifyListeners();
   }
 }
--- lib/data_source/data_helper.dart
+++ lib/data_source/data_helper.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
-
+import 'package:dokkan_firebase/model/deposite_model.dart';
+import 'package:http/http.dart' as http;
 String baseUrl55 = 'https://expenses.dokkan.xyz/api/v1';
 
 class DbHelper {
@@ -45,13 +46,25 @@ class DbHelper {
   //   }
   // }
 
+  Future<DepositeModel> getDataOmar({required String  date}) async {
+    var response = await http.get(Uri.parse('$baseUrl55/all_deposit_for_app/$date'));
+    DepositeModel? detailModel ;
+    if (response.statusCode == 200 || response.statusCode==201 ) {
+      // try {
+        final json = jsonDecode(const Utf8Decoder().convert(response.bodyBytes));
+        detailModel = DepositeModel.fromJson(json);
+      // } catch (e) {
+      // }
+    }
+    print("response>>"+response.body);
+    return detailModel!;
+  }
   Future<Response?> getDataSource({
     required String  date}
   ) async {
     print('get Dio++++');
-
     var response = await dio.get('$baseUrl55/all_deposit_for_app/$date',);
-    print('response..');
+    print('responsedddd>>'+response.data.toString());
     if (response.statusCode == 200 || response.statusCode == 201) {
       return response;
     } else {
